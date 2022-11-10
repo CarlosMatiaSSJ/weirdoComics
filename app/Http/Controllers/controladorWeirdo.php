9cb482062ac5b8b9b0be07841eb3756230ea2f87
Ac-Arreola <120034358@upq.edu.mx>
2022-11-09 19:34:10 -0600

    Controlador merge

--- app/Http/Controllers/controladorWeirdo.php
+++ app/Http/Controllers/controladorWeirdo.php
@@ -5,9 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\validadorWeirdo;
 use App\Http\Requests\validadorWeirdoActualizarComic;
+use App\Http\Requests\validadorWeirdoActualizarArticulo;
 use App\Http\Requests\validadorWeirdoAgregarArticulo;
+use App\Http\Requests\validadorWeirdoAgregarPedido;
 use App\Http\Requests\validadorWeirdoAgregarComic;
-use App\Http\Requests\validadorAgregarArticulo;
 
 class controladorWeirdo extends Controller
 {
@@ -23,19 +24,32 @@ class controladorWeirdo extends Controller
 
     public function confirmarActualizacionComic(validadorWeirdoActualizarComic $req)
     {
-        return redirect('comics')->with('actualizacion', 'comic Recibida');
+        return redirect('comics')->with('actualizacion', 'comic Recibido');
+    }
+    public function confirmarActualizacionArticulo(validadorWeirdoActualizarArticulo $req)
+    {
+        return redirect('articulos')->with('actualizacion', 'articulo Recibido');
     }
 
     public function confirmarArticulo(validadorWeirdoAgregarArticulo $req)
     {
         return redirect('articulos')->with('articuloAgregado', 'Articulo Recibida');
     }
-
+    public function confirmarPedido(validadorWeirdoAgregarPedido $req)
+    {
+        return redirect('pedidos')->with('pedidoAgregado', 'Articulo Recibida');
+    }
 
 
     public function showLogin()
     {
         return view('login');
+    }
+
+
+    public function showInventario()
+    {
+        return view('inventario');
     }
 
     public function showIndex()
@@ -56,6 +70,25 @@ class controladorWeirdo extends Controller
     public function showEditarComic()
     {
         return view('editarComic');
+    }
+
+    public function showPedidos()
+    {
+        return view('pedido');
+    }
+    public function showAgregarPedido()
+    {
+        return view('agregarPedido');
+    }
+
+    public function showPuntoVenta()
+    {
+        return view('pVenta');
+    }
+
+    public function showEditarArticulo()
+    {
+        return view('editarArticulo');
     }
 
     public function showArticulos()
